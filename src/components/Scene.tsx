fcf50645de614966b4b7315a3c1afee2fa96bcfa
pampa2025 <pampa@126.com>
2025-01-29 20:30:03 +0800

    init anime like material

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,27 +1,37 @@
-import { useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
-import { Mesh } from 'three'
-import { useStore } from '../store'
+import { Canvas } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Mesh } from 'three';
+import { Color } from 'three';
+import { useFrame } from '@react-three/fiber';
 
 const Scene = () => {
-console.log('scene init')
-const meshRef = useRef<Mesh>(null)
-const rotation = useStore((state: { rotation: number }) => state.rotation)
-const setRotation = useStore((state: { setRotation: (rotation: number) => void }) => state.setRotation)
+	console.log('scene init');
+	const meshRef = useRef<Mesh>(null);
+	const rotationSpeed = 0.01;
 
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y = rotation
-      setRotation(rotation + 0.01)
-    }
-  })
+	useFrame(() => {
+		if (meshRef.current) {
+			meshRef.current.rotation.y += rotationSpeed;
+		}
+	});
 
-  return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  )
-}
-
-export default Scene
+	return (
+		<>
+			{/* Main object */}
+			<mesh ref={meshRef}>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshToonMaterial
+					color={new Color('#FF9B50')}
+					emissive={new Color('#FF9B50')}
+					emissiveIntensity={0.2}
+				/>
+			</mesh>
+			{/* Outline mesh */}
+			<mesh ref={meshRef} scale={1.05}>
+				<boxGeometry args={[1, 1, 1]} />
+				<meshBasicMaterial color="black" side={2} transparent opacity={0.8} />
+			</mesh>
+		</>
+	);
+};
+export default Scene;
