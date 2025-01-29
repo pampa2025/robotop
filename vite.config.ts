969e06a1f273657fccf51a60a57f49196e72a79a
pampa2025 <pampa@126.com>
2025-01-29 12:59:00 +0000

    add config for marscode ide web

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['h3vpzaeq-y3swm9ke-iwmtm82sj1s7.aca2-preview.marscode.dev'],
+  },
 })
